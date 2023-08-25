66f1569145a258aa4d7bff81a1ea281b47efff51
Christine Vu <okiedokie18@gmail.com>
2023-08-24 20:37:55 -0400

    hamburger menu fixes

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -9,9 +9,11 @@ export default function Header(props: HeaderProps) {
     return (
         <div>
             <Menu>
-                <a id="home" className="menu-item" href="/">Home</a>
-                <a id="about" className="menu-item" href="/about">About</a>
-                <a id="contact" className="menu-item" href="/contact">Contact</a>
+                <a id="home" className="menu-item" href="/">Teas Offered</a>
+                <a id="about" className="menu-item" href="/about">History</a>
+                <a id="contact" className="menu-item" href="/contact">Events</a>
+                <a id="about" className="menu-item" href="/about">Shop</a>
+                <a id="contact" className="menu-item" href="/contact">Contact Us</a>
             </Menu>
             <div className="Header" style={{backgroundColor: props.color}}>
                 <div className="header-title">
